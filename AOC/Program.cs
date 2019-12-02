f4a4f7eeb622a0a6a8c53f4b8d5df5c1ec9d9d87
Charles Custer <ccuster@iprotech.com>
2019-12-02 09:35:55 -0800

    puzzle01

---
+++ AOC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC
+{
+    class Program
+    {
+        public static void calculate(int[] puzzleArr)
+        {
+            var sum = 0;
+            foreach (var i in puzzleArr)
+            {
+                sum += (i / 3) - 2;
+            }
+
+            Console.WriteLine(sum);
+        }
+
+
+        static void Main(string[] args)
+        {
+            var testData = @"h:\git\adventofcode\puzzle01.txt";
+            var numInArr = 100;
+            testData = @"h:\git\adventofcode\puzzle01.txt";
+
+            using (TextReader tr = new StreamReader(testData))
+            {
+                var puzzleArr = new int[numInArr];
+                for (int i = 0; i < puzzleArr.Length; i++)
+                {
+                    puzzleArr[i] = int.Parse(tr.ReadLine());
+                }
+
+                calculate(puzzleArr);
+            }
+
+        }
+    }
+}
