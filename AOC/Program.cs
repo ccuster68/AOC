125b6bd4e2ac5012359c08973909aa49e997bcc5
Charles Custer <ccuster@iprotech.com>
2019-12-02 13:36:45 -0800

    D2P1 finished

--- AOC/Program.cs
+++ AOC/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AOC
 {
@@ -22,26 +24,47 @@ namespace AOC
         static void Main(string[] args)
         {
             var testBase = @"h:\git\aoc\testfiles";
-            var testData = Path.Combine(testBase, "puzzle02.txt");
-            var numInArr = 100;
+            var testData = Path.Combine(testBase, "d2p1O.txt");
             var sum = 0;
 
+            var items = new List<int>();
             using (TextReader tr = new StreamReader(testData))
             {
-                var puzzleArr = new int[numInArr];
-                for (int i = 0; i < puzzleArr.Length; i++)
+                var value = tr.ReadLine();
+                var arr = value.Split(',').Select(int.Parse).ToArray();
+                arr[1] = 12;
+                arr[2] = 2;
+                for (var i = 0; i < arr.Length; i += 4)
                 {
-                    puzzleArr[i] = int.Parse(tr.ReadLine());
+                    var op = arr[i];
+                    if (op == 99)
+                        break;
+                    var idx1 = arr[i + 1];
+                    var idx2 = arr[i + 2];
+                    var idx3 = arr[i + 3];
+
+                    switch (op)
+                    {
+                        case 1:
+                            arr[idx3] = arr[idx1] + arr[idx2];
+                            break;
+                        case 2:
+                            arr[idx3] = arr[idx1] * arr[idx2];
+                            break;
+                    }
+
                 }
 
-                foreach (var i in puzzleArr)
+                for (int i = 0; i < arr.Length; i++)
                 {
-                    sum += Calculate(i);
+                    var toWrite= i == arr.Length - 1? $"{arr[i]}": $"{arr[i]},";
+                    Console.Write(toWrite);
                 }
-                
+
+                Console.WriteLine();
             }
 
-            Console.WriteLine(sum);
+            
 
         }
     }
